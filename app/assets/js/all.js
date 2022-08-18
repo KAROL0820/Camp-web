1164a222f5753c6e918fbf9b429412ba955406b6
KAROL0820 <karolinee0820@gmail.com>
2022-08-18 20:24:53 +0800

    8/18 update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,18 @@
 $(function() {
   console.log('Hello Bootstrap5');
 });
+
+//SWIPER
+var swiperTestimonial = new Swiper(".swiper--testimonial", {
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  navigation: {
+    nextEl: ".function__next",
+    prevEl: ".function__prev",
+  },
+  pagination: {
+    el: ".function__pagination",
+  },
+});
