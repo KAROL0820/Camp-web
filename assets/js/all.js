4eb765686ea16c9953c74025349e95d0d118f545
KAROL0820 <karolinee0820@gmail.com>
2022-08-18 20:24:27 +0800

    Update 2022-08-18T12:24:24.429Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -2,5 +2,19 @@
 
 $(function () {
   console.log('Hello Bootstrap5');
+}); //SWIPER
+
+var swiperTestimonial = new Swiper(".swiper--testimonial", {
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  navigation: {
+    nextEl: ".function__next",
+    prevEl: ".function__prev"
+  },
+  pagination: {
+    el: ".function__pagination"
+  }
 });
 //# sourceMappingURL=all.js.map
